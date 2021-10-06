dccd344d8bbaf8c433d7a8fdb8dfb544a765e27f
Orchit07 <orchitiadi@gmail.com>
2021-10-06 13:43:15 +0700

    Rewrite to Typescript

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.erelaFilters = void 0;
+const Plugin_1 = require("./Plugin");
+Object.defineProperty(exports, "erelaFilters", { enumerable: true, get: function () { return Plugin_1.erelaFilters; } });
