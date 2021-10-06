761cf91298d7b57a074c12b5a681760302064b7d
KChi <59391215+KagChi@users.noreply.github.com>
2021-10-06 18:37:37 +0700

    initial commit

--- src/data/MainSource.js
+++ src/data/MainSource.js
@@ -3,8 +3,8 @@ import DocsManager from "./DocsManager";
 const blacklisted = new Set(["docs", "docs-website"]);
 
 export default new DocsManager({
-    id: "main",
-    name: "Main",
+    id: "erelaFilter",
+    name: "Main Library",
     global: "Main",
     repo: "NezuChan/erela.js-filter",
     defaultTag: "main",
